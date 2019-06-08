064c659fc470d7df6b809b62c01eedc076f7d97f
chenxue <melthaw@gmail.com>
2019-06-08 11:31:36 +0800

    add more profile

--- src/main/java/io/picos/helloworld/HelloWorldApplication.java
+++ src/main/java/io/picos/helloworld/HelloWorldApplication.java
@@ -3,10 +3,18 @@ package io.picos.helloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class HelloWorldApplication {
+
+    @Configuration
+    @EnableEurekaClient
+    @Profile({"production", "development"})
+    public class EurekaConfigurer {
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
