89f0de23babd729d9a1797c136117b4c9fbc6c12
chenxue <melthaw@gmail.com>
2019-06-09 09:53:52 +0800

    fixed compile

--- src/main/java/io/picos/helloworld/controller/SleepRestController.java
+++ src/main/java/io/picos/helloworld/controller/SleepRestController.java
@@ -1,15 +1,17 @@
 package io.picos.helloworld.controller;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.tags.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SleepRestController {
 
     @GetMapping("/sleep")
     @ResponseStatus(HttpStatus.OK)
-    public void sleep(@@RequestParam("duration") int duration) {
+    public void sleep(@RequestParam("duration") int duration) {
         try {
             Thread.sleep(duration);
         } catch (InterruptedException e) {
